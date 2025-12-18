9ab1a793aaade67c50ecbad49c0566fbcfa5f286
Alejandro Ball <aball615@icloud.com>
2025-12-18 18:33:35 +0000

    expanded testing fields.

--- main.c
+++ main.c
@@ -19,6 +19,14 @@ int	main(void)
 	nulltest = ft_printf(NULL);
 	ft_printf("null test = %i.\n", nulltest);
 
+	ft_printf("pointer ref for string phrase: %p\n", &phrase);
+	ft_printf("Hexadecimal negative lower case: %x\n", 42);
+	ft_printf("Hexadecimal negative upper case: %X\n", 42);
+	ft_printf("Hexadecimal negative lower case: %x\n", -42);
+	ft_printf("Hexadecimal negative upper case: %X\n", -42);
+
+	ft_printf("\n");
+
 	printf("/************ printf tests ************/\n");
 	printf("Basic print, no value passed test\n");
 	printf("Hello World, my I am %d years old.\n", age);
@@ -35,6 +43,10 @@ int	main(void)
 	printf("pointer ref for string phrase: %p\n", &phrase);
 	printf("Hexadecimal lower case: %x\n", age);
 	printf("Hexadecimal upper case: %X\n", age);
+	printf("Hexadecimal negative lower case: %x\n", -42);
+	printf("Hexadecimal negative upper case: %X\n", -42);
+
+	printf("\n");
 
 	return (0);
 }
