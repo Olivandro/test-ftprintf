bbda74007e6bcbd748185bfd677ce427a9ef6bd6
Alejandro Ball <aball615@icloud.com>
2025-12-28 16:51:46 +0000

    added a few more tests for comparison

--- main.c
+++ main.c
@@ -94,7 +94,16 @@ int	main(void)
 
 
 	printf("\n");
-
-
+	int count = ft_printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+	printf("\n");
+	printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+	printf("\n");
+	ft_printf("%d", count);
+	printf("\n");
+	ft_printf("%x\n", 0);
+	ft_printf("%x\n", LONG_MIN);
+	printf("%lx\n", LONG_MIN);
+	printf("%p\n", 0);
+	printf("%s\n", NULL);
 	return (0);
 }
